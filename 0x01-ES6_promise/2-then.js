1ee6a9b4585c79b134316a7f3f49e5bfbdeea942
me <me>
2024-08-07 13:37:38 +0100

    promise_fix_2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
-// 2-then.js
 function handleResponseFromAPI(promise) {
-    promise
-        .then((response) => {
-            console.log("Got a response from the API");
-            return { status: 200, body: "success" };
-        })
-        .catch((error) => {
-            return new Error();
-        });
+  const response = { status: 200, body: 'success' };
+  return promise
+    .then(() => response)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
