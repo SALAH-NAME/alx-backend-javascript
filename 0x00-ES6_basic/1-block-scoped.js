967544d9da74feb5a84b221ec7932ff4c7d0b490
me <me>
2024-08-09 10:57:11 +0100

    basic_fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
-function taskBlock(trueOrFalse) {
-  const task1 = false;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task1 = true;
+    const task = true;
     const task2 = false;
   }
 
-  return [task1, task2];
+  return [task, task2];
 }
-
-export default taskBlock;
