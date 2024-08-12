f6dbfe12f8dec56fa0e8e098b1e7dfe172d09efb
Your Name <you@example.com>
2024-08-12 15:26:44 +0000

    data_manipulation_fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
 const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const array = new Int8Array(buffer);
-  if (position > array.length)
+  if (position > array.length) {
     throw new Error('Position outside range');
+  }
   array[position] = value;
   return new DataView(buffer);
 };
