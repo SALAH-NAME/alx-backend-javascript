d6f2f1849a2c682f9e213c623858bd2af0404152
Salah <Salah>
2024-10-02 22:43:23 +0100

    unittests_in_js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,12 @@
-const request = require('request');
+const httpRequest = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
-  const url = 'http://localhost:7865/';
+describe('Payment API Integration Test', () => {
+  const BASE_URL = 'http://localhost:7865';
 
-  it('should return status code 200', (done) => {
-    request(url, (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct message', (done) => {
-    request(url, (error, response, body) => {
+  it('GET / should return the correct response', (done) => {
+    httpRequest.get(`${BASE_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
